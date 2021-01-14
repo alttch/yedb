32ad8c4a39fcb556d83b64c7a9ccc62acaccdd3e
Sergei S <div@altertech.com>
2021-01-14 05:37:09 +0100

    dark style

--- convert.py
+++ convert.py
@@ -9,11 +9,12 @@ build_dir = Path('_build')
 build_dir.mkdir(exist_ok=True)
 
 with open('README.md') as fh:
-    md = markdown.markdown(fh.read(), extensions=['markdown.extensions.tables'])
+    readme = markdown.markdown(fh.read(),
+                               extensions=['markdown.extensions.tables'])
 
 with open('index.j2') as fh:
     tpl = jinja2.Template(fh.read())
 
-(build_dir / 'index.htm').write_text(tpl.render(dict(md=md)))
+(build_dir / 'index.html').write_text(tpl.render(dict(readme=readme)))
 
 shutil.copy('yedb.jpg', build_dir)
